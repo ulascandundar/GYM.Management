8bd9eb3e983ea3004b854f6a2584b9d0d3b4dbde
ulascandundar <ulas.dundar@picksoft.com.tr>
2023-07-29 15:13:34 +0300

    developed

--- src/GYM.Management.Application.Contracts/Members/MemberCreateDto.cs
+++ src/GYM.Management.Application.Contracts/Members/MemberCreateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace GYM.Management.Members
@@ -8,6 +9,7 @@ namespace GYM.Management.Members
     {
         public string Name { get; set; }
         public DateTime BirdthDate { get; set; }
+        [MinLength(10, ErrorMessage = "Telefon 10 haneden az olamaz"), MaxLength(11, ErrorMessage = "Telefon 11 haneden fazla olamaz")]
         public string Telephone { get; set; }
         public Guid? TrainerId { get; set; }
     }
